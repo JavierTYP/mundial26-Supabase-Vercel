e97cadab89fd14040ada213570fa93eaeda803d0
VAZQUEZ GIL JAVIER <fjvazquez@typsa.es>
2026-06-02 11:30:06 +0200

    Use Golden Boot players for Golden Ball picks

--- src/views/MvpView.jsx
+++ src/views/MvpView.jsx
@@ -5,7 +5,7 @@ import SelectMenu from "../components/SelectMenu.jsx";
 import { parseCsv } from "../utils/csv.js";
 import { apiGetMyMvp, apiPutMyMvp } from "../utils/api.js";
 import { loadMvp, saveMvp } from "../utils/mvpStorage.js";
-import jugadoresCsv from "../../data/jugadores.csv?raw";
+import goleadoresCsv from "../../data/goleadores.csv?raw";
 
 function normalizePick(pick) {
   const row = pick && typeof pick === "object" ? pick : {};
@@ -17,7 +17,7 @@ function normalizePick(pick) {
 
 export default function MvpView({ userEmail, predictionsLocked = false }) {
   const { teamsByGroup, playersByTeam } = useMemo(() => {
-    const rows = parseCsv(jugadoresCsv);
+    const rows = parseCsv(goleadoresCsv);
     const byGroup = new Map();
     const players = new Map();
 
